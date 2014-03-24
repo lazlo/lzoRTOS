afb9326d537fc35f1d31335ead817e0f8df33e39
Daniel Laszlo Sitzer <dlsitzer@gmail.com>
2014-03-24 23:44:58 +0100

    Added code for avr spi init and send.

--- lzortos/arch/avr/avr_spi.c
+++ lzortos/arch/avr/avr_spi.c
@@ -1,3 +1,19 @@
+#include "pins.h"
+
 void avr_spi_init(void)
 {
+	/* Set MOSI and SCK output, all others input */
+	SPI_MOSI_DDR |= (1 << SPI_MOSI_OFFSET);
+	SPI_SCK_DDR |= (1 << SPI_SCK_OFFSET);
+	/* Enable SPI, Master, set clock rate fck/16 */
+	SPCR = (1 << SPE)|(1 << MSTR)|(1 << SPR0);
+}
+
+void avr_spi_send(const char c)
+{
+	/* Start transmission */
+	SPDR = c;
+	/* Wait for transmission complete */
+	while (!(SPSR & (1 << SPIF)))
+		;
 }
--- lzortos/arch/avr/avr_spi.h
+++ lzortos/arch/avr/avr_spi.h
@@ -3,4 +3,6 @@
 
 void avr_spi_init(void);
 
+void avr_spi_send(const char c);
+
 #endif /* D_ARCH_AVR_SPI_H */
