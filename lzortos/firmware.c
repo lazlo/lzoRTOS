41f3b48a93ceb0430c197d896628d435c586e587
Daniel Laszlo Sitzer <dlsitzer@gmail.com>
2014-03-24 23:04:26 +0100

    Added stub for filesystem.

--- lzortos/firmware.c
+++ lzortos/firmware.c
@@ -2,6 +2,7 @@
 #include "avr_usart.h"
 #include "board.h"
 #include "enc28j60.h"
+#include "fs.h"
 #include "sched.h"
 
 void _putc(const char c)
@@ -20,6 +21,7 @@ int main(void)
 	chip_init();
 	board_init();
 	enc28j60_init();
+	fs_init();
 	sched_init();
 	_puts("ready!\r\n");
 }
---
+++ lzortos/fs/fs.h
@@ -0,0 +1,6 @@
+#ifndef D_FS_H
+#define F_FS_H
+
+void fs_init(void);
+
+#endif /* D_FS_H */
