8afff9f1e3ecaeecd5b18720a0eabeb09036ee0e
Daniel Laszlo Sitzer <dlsitzer@gmail.com>
2014-03-28 01:32:43 +0100

    Integrated timer1 compare match interrupt.

--- lzortos/firmware.c
+++ lzortos/firmware.c
@@ -1,9 +1,12 @@
 #include "debug.h"
+#include "pins.h"
 #include "chip.h"
 #include "avr_usart.h"
 #include "board.h"
 #include "fs.h"
 #include "sched.h"
+
+#include <avr/interrupt.h>
 
 void hw_init(void)
 {
@@ -23,4 +26,29 @@ int main(void)
 {
 	hw_init();
 	os_start();
+
+	sei();
+
+	while (1)
+		;
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+	static int i = 0;
+
+	if (i < 100) {
+		i++;
+	} else {
+		i = 0;
+		if (LED1_PORT & (1 << LED1_OFFSET)) {
+			LED1_PORT &= ~(1 << LED1_OFFSET);
+			LED2_PORT &= ~(1 << LED2_OFFSET);
+			LED3_PORT &= ~(1 << LED3_OFFSET);
+		} else {
+			LED1_PORT |= (1 << LED1_OFFSET);
+			LED2_PORT |= (1 << LED2_OFFSET);
+			LED3_PORT |= (1 << LED3_OFFSET);
+		}
+	}
 }
