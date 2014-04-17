ed38d5470f380f6fc164b847bdd470b59c663c2f
Daniel Laszlo Sitzer <dlsitzer@gmail.com>
2014-04-18 00:05:23 +0200

    config: Added comments to options

--- lzortos/config.h
+++ lzortos/config.h
@@ -1,14 +1,20 @@
 #ifndef D_CONFIG_H
 #define D_CONFIG_H
 
+/* Enable debug output. */
 //#define CONFIG_DEBUG
 
+/* Enable the basic co-operative scheduler. */
 #define CONFIG_SCHED
 
+/* Enable support for I2C peripherals. */
 //#define CONFIG_I2C
+/* Enable support for ADC peripherals. */
 //#define CONFIG_ADC
+/* Enable filesystem support. */
 //#define CONFIG_FS
 
+/* Maximum lengh of a command line. */
 #define CONFIG_LSH_GETLINE_NCHARS 80
 
 #endif /* D_CONFIG_H */
