1ebfd996848e8e79b1ae0ae1f057d1a3f7180670
Daniel Laszlo Sitzer <dlsitzer@gmail.com>
2014-04-17 23:47:28 +0200

    sched: Added subs for basic scheduler functions.

--- lzortos/sched/sched.c
+++ lzortos/sched/sched.c
@@ -4,3 +4,20 @@ void sched_init(void)
 {
 	dbg("sched: ready!\r\n");
 }
+
+/* Enable the scheduler. */
+void sched_start(void)
+{
+}
+
+/* Schedule tasks for execution.
+ * Needs to be called periodically. */
+void sched_update(void)
+{
+}
+
+/* Execute scheduled tasks
+ * Needs to be called periodically. */
+void sched_dispatch_tasks(void)
+{
+}
--- lzortos/sched/sched.h
+++ lzortos/sched/sched.h
@@ -3,4 +3,10 @@
 
 void sched_init(void);
 
+void sched_start(void);
+
+void sched_update(void);
+
+void sched_dispatch_tasks(void);
+
 #endif /* D_SCHED_H */
