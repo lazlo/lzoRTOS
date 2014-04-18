a7201b662417cd1a54d763014d7f88e63ba9a84e
Daniel Laszlo Sitzer <dlsitzer@gmail.com>
2014-04-18 16:36:46 +0200

    lsh: Maded ps stub list only valid entries from task list.

--- apps/lsh/lsh_cmd_ps.c
+++ apps/lsh/lsh_cmd_ps.c
@@ -1,11 +1,17 @@
 #include "config.h"
 #include "lsh_internal.h"
 
+#include "sched.h"
+
+extern struct task g_sched_task[];
+
 void lsh_cmd_ps(void)
 {
 	int i;
 
 	lsh_puts("PID TTY TIME CMD\r\n");
-	for (i = 0; i < CONFIG_SCHED_NTASKS; i++)
-		lsh_puts("0   pts/0 00:00:00 foo\r\n");
+	for (i = 0; i < CONFIG_SCHED_NTASKS; i++) {
+		if (g_sched_task[i].fp != 0)
+			lsh_puts("0   pts/0 00:00:00 foo\r\n");
+	}
 }
