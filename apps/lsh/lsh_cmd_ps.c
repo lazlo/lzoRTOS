f42def9e3739a81cba2886c00932c7a4dd30e9f7
Daniel Laszlo Sitzer <dlsitzer@gmail.com>
2014-04-18 19:37:47 +0200

    lsh: Stub for PID in ps.

--- apps/lsh/lsh_cmd_ps.c
+++ apps/lsh/lsh_cmd_ps.c
@@ -8,10 +8,19 @@ extern struct task g_sched_task[];
 void lsh_cmd_ps(void)
 {
 	int i;
+	char pidstr[3];
 
 	lsh_puts("PID TTY TIME CMD\r\n");
 	for (i = 0; i < CONFIG_SCHED_NTASKS; i++) {
-		if (g_sched_task[i].fp != 0)
-			lsh_puts("0   pts/0 00:00:00 foo\r\n");
+		if (g_sched_task[i].fp != 0) {
+			pidstr[0] = '0' + i;
+			pidstr[1] = '\0';
+
+			lsh_puts(pidstr);
+			lsh_puts("   ");
+			lsh_puts("pts/0 ");
+			lsh_puts("00:00:00 ");
+			lsh_puts("foo\r\n");
+		}
 	}
 }
