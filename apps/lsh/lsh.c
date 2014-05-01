7faeec55157325028bfffe7b1ec1813a6c0477f6
Daniel Laszlo Sitzer <dlsitzer@gmail.com>
2014-05-01 22:54:35 +0200

    config: Added extra symbols to enable lsh commands individually.

--- apps/lsh/lsh.c
+++ apps/lsh/lsh.c
@@ -55,107 +55,207 @@ static int lsh_strncmp(char *s1, char *s2, const int limit)
 static void lsh_cmd_help(void)
 {
 #ifdef CONFIG_NET
+#ifdef CONFIG_CMD_ARP
 	lsh_puts(" arp\r\n");
 #endif
+#endif
 #ifdef CONFIG_FS
+#ifdef CONFIG_CMD_CAT
 	lsh_puts(" cat\r\n");
+#endif
+#ifdef CONFIG_CMD_CD
 	lsh_puts(" cd\r\n");
+#endif
+#ifdef CONFIG_CMD_CP
 	lsh_puts(" cp\r\n");
+#endif
+#ifdef CONFIG_CMD_DD
 	lsh_puts(" dd\r\n");
+#endif
+#ifdef CONFIG_CMD_DU
 	lsh_puts(" du\r\n");
+#endif
+#ifdef CONFIG_CMD_DF
 	lsh_puts(" df\r\n");
 #endif
+#endif
 #if 0
+#ifdef CONFIG_CMD_ECHO
 	lsh_puts(" echo\r\n");
 #endif
+#endif
 #ifdef CONFIG_NET
+#ifdef CONFIG_CMD_ETHTOOL
 	lsh_puts(" ethtool\r\n");
 #endif
+#endif
 #if 0
+#ifdef CONFIG_CMD_ENV
 	lsh_puts(" env\r\n");
+#endif
+#ifdef CONFIG_CMD_FREE
 	lsh_puts(" free\r\n");
+#endif
+#ifdef CONFIG_CMD_GREP
 	lsh_puts(" grep\r\n");
+#endif
+#ifdef CONFIG_CMD_HALT
 	lsh_puts(" halt\r\n");
+#endif
+#ifdef CONFIG_CMD_HEAD
 	lsh_puts(" head\r\n");
+#endif
 #endif
 	lsh_puts(" help\r\n");
 #ifdef CONFIG_NET
+#ifdef CONFIG_CMD_HOSTNAME
 	lsh_puts(" hostname\r\n");
 #endif
+#endif
 #if 0
+#ifdef CONFIG_CMD_HWCLOCK
 	lsh_puts(" hwclock\r\n");
 #endif
+#endif
 #ifdef CONFIG_I2C
+#ifdef CONFIG_CMD_I2C
 	lsh_puts(" i2c\r\n");
 #endif
+#endif
 #ifdef CONFIG_NET
+#ifdef CONFIG_CMD_IFCONFIG
 	lsh_puts(" ifconfig\r\n");
 #endif
+#endif
 #ifdef CONFIG_SCHED
+#ifdef CONFIG_CMD_KILL
 	lsh_puts(" kill\r\n");
 #endif
+#endif
 #if 0
+#ifdef CONFIG_CMD_LESS
 	lsh_puts(" less\r\n");
 #endif
+#endif
 #ifdef CONFIG_FS
+#ifdef CONFIG_CMD_LS
 	lsh_puts(" ls\r\n");
 #endif
+#endif
 #ifdef CONFIG_NET
+#ifdef CONFIG_CMD_MIITOOL
 	lsh_puts(" mii-tool\r\n");
 #endif
+#endif
 #ifdef CONFIG_FS
+#ifdef CONFIG_CMD_MOUNT
 	lsh_puts(" mount\r\n");
+#endif
+#ifdef CONFIG_CMD_MKDIR
 	lsh_puts(" mkdir\r\n");
+#endif
+#ifdef CONFIG_CMD_MV
 	lsh_puts(" mv\r\n");
 #endif
+#endif
 #ifdef CONFIG_NET
+#ifdef CONFIG_CMD_NETSTAT
 	lsh_puts(" netstat\r\n");
+#endif
+#ifdef CONFIG_CMD_NETCAT
 	lsh_puts(" netcat\r\n");
+#endif
+#ifdef CONFIG_CMD_PING
 	lsh_puts(" ping\r\n");
 #endif
+#endif
 #ifdef CONFIG_SCHED
+#ifdef CONFIG_CMD_PS
 	lsh_puts(" ps\r\n");
 #endif
+#endif
 #ifdef CONFIG_FS
+#ifdef CONFIG_CMD_PWD
 	lsh_puts(" pwd\r\n");
+#endif
+#ifdef CONFIG_CMD_RM
 	lsh_puts(" rm\r\n");
+#endif
+#ifdef CONFIG_CMD_RMDIR
 	lsh_puts(" rmdir\r\n");
 #endif
+#endif
 #ifdef CONFIG_NET
+#ifdef CONFIG_CMD_ROUTE
 	lsh_puts(" route\r\n");
+#endif
+#ifdef CONFIG_CMD_SS
 	lsh_puts(" ss\r\n");
 #endif
+#endif
 #ifdef CONFIG_SPI
+#ifdef CONFIG_CMD_SPI
 	lsh_puts(" spi\r\n");
 #endif
+#endif
 #ifdef CONFIG_FS
+#ifdef CONFIG_CMD_STRINGS
 	lsh_puts(" strings\r\n");
+#endif
+#ifdef CONFIG_CMD_TAIL
 	lsh_puts(" tail\r\n");
 #endif
+#endif
 #ifdef CONFIG_NET
+#ifdef CONFIG_CMD_TCPDUMP
 	lsh_puts(" tcpdump\r\n");
+#endif
+#ifdef CONFIG_CMD_TELNET
 	lsh_puts(" telnet\r\n");
 #endif
+#endif
 #ifdef CONFIG_FS
+#ifdef CONFIG_CMD_TOUCH
 	lsh_puts(" touch\r\n");
+#endif
+#ifdef CONFIG_CMD_TTY
 	lsh_puts(" tty\r\n");
 #endif
+#endif
 #ifdef CONFIG_NET
+#ifdef CONFIG_CMD_TRACEROUTE
 	lsh_puts(" traceroute\r\n");
 #endif
+#endif
 #if 0
+#ifdef CONFIG_CMD_UPTIME
 	lsh_puts(" uptime\r\n");
 #endif
+#endif
 #ifdef CONFIG_FS
+#ifdef CONFIG_CMD_UMOUNT
 	lsh_puts(" umount\r\n");
 #endif
+#endif
 #if 0
+#ifdef CONFIG_CMD_UNAME
 	lsh_puts(" uname\r\n");
+#endif
+#ifdef CONFIG_CMD_WGET
 	lsh_puts(" wget\r\n");
+#endif
+#ifdef CONFIG_CMD_XD
 	lsh_puts(" xd\r\n");
+#endif
+#ifdef CONFIG_CMD_MB
 	lsh_puts(" mb\r\n");
+#endif
+#ifdef CONFIG_CMD_MH
 	lsh_puts(" mh\r\n");
+#endif
+#ifdef CONFIG_CMD_MW
 	lsh_puts(" mw\r\n");
+#endif
 #endif
 }
 
--- lzortos/config.h
+++ lzortos/config.h
@@ -37,4 +37,57 @@
 /* Maximum lengh of a command line. */
 #define CONFIG_LSH_GETLINE_NCHARS 80
 
+/* LSH Commands */
+
+//#define CONFIG_CMD_ARP
+//#define CONFIG_CMD_CAT
+//#define CONFIG_CMD_CD
+//#define CONFIG_CMD_CP
+//#define CONFIG_CMD_DD
+//#define CONFIG_CMD_DU
+//#define CONFIG_CMD_DF
+//#define CONFIG_CMD_ECHO
+//#define CONFIG_CMD_ETHTOOL
+//#define CONFIG_CMD_ENV
+//#define CONFIG_CMD_FREE
+//#define CONFIG_CMD_GREP
+//#define CONFIG_CMD_HALT
+//#define CONFIG_CMD_HEAD
+//#define CONFIG_CMD_HOSTNAME
+//#define CONFIG_CMD_HWCLOCK
+//#define CONFIG_CMD_I2C
+//#define CONFIG_CMD_IFCONFIG
+#define CONFIG_CMD_KILL
+//#define CONFIG_CMD_LESS
+//#define CONFIG_CMD_LS
+//#define CONFIG_CMD_MIITOOL
+//#define CONFIG_CMD_MOUNT
+//#define CONFIG_CMD_MKDIR
+//#define CONFIG_CMD_MV
+//#define CONFIG_CMD_NETSTAT
+//#define CONFIG_CMD_NETCAT
+//#define CONFIG_CMD_PING
+#define CONFIG_CMD_PS
+//#define CONFIG_CMD_PWD
+//#define CONFIG_CMD_RM
+//#define CONFIG_CMD_RMDIR
+//#define CONFIG_CMD_ROUTE
+//#define CONFIG_CMD_SS
+#define CONFIG_CMD_SPI
+//#define CONFIG_CMD_STRINGS
+//#define CONFIG_CMD_TAIL
+//#define CONFIG_CMD_TCPDUMP
+//#define CONFIG_CMD_TELNET
+//#define CONFIG_CMD_TOUCH
+//#define CONFIG_CMD_TTY
+//#define CONFIG_CMD_TRACEROUTE
+//#define CONFIG_CMD_UPTIME
+//#define CONFIG_CMD_UMOUNT
+//#define CONFIG_CMD_UNAME
+//#define CONFIG_CMD_WGET
+//#define CONFIG_CMD_XD
+//#define CONFIG_CMD_MB
+//#define CONFIG_CMD_MH
+//#define CONFIG_CMD_MW
+
 #endif /* D_CONFIG_H */
