93da7327df856a372166fdb15e66b51ecb6a61e5
Daniel Laszlo Sitzer <dlsitzer@gmail.com>
2014-05-03 00:11:17 +0200

    lsh: Make ifconfig read the rx packet count from global var defined in net.c.

--- apps/lsh/lsh_cmd_ifconfig.c
+++ apps/lsh/lsh_cmd_ifconfig.c
@@ -1,13 +1,25 @@
 #include "config.h"
 #include "lsh_internal.h"
 
+#include "strutils.h"
+
+extern unsigned short g_eth0_rxpktcnt;
+
 void lsh_cmd_ifconfig(void)
 {
+	unsigned short rxpktcnt = 0;
+	char str_rxpktcnt[6];
+
+	rxpktcnt = g_eth0_rxpktcnt;
+	itoa(rxpktcnt, str_rxpktcnt, 10);
+
 	lsh_puts("eth0      ");
 	lsh_puts("Link encap:Ethernet  HWaddr 00:00:00:00:00:00\r\n");
 	lsh_puts("          ");
 	lsh_puts("RX ");
-	lsh_puts("packets:0 ");
+	lsh_puts("packets:");
+	lsh_puts(str_rxpktcnt);
+	lsh_puts(" ");
 	lsh_puts("errors:0 ");
 	lsh_puts("dropped:0 ");
 	lsh_puts("overruns:0 ");
