e5b7a435824ce35fec399518e92581812be028f2
Daniel Laszlo Sitzer <dlsitzer@gmail.com>
2014-05-03 19:02:37 +0200

    lzodso-disp: Added stub source and header files.

---
+++ lzortos/configs/lzodso-disp/lzodso-disp.c
@@ -0,0 +1,11 @@
+#include "config.h"
+#include "pins.h"
+
+static void lzodso_disp_init(void)
+{
+}
+
+int board_init(void)
+{
+	lzodso_disp_init();
+}
---
+++ lzortos/configs/lzodso-disp/lzodso-disp.h
@@ -0,0 +1,6 @@
+#ifndef D_LZODSO_DISP_H
+#define D_LZODSO_DISP_H
+
+int board_init(void);
+
+#endif /* D_LZODSO_DISP_H */
