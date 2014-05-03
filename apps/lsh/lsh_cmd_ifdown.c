0e8a1313e16ff00f3b75bd7d518df452e7983ddf
Daniel Laszlo Sitzer <dlsitzer@gmail.com>
2014-05-03 19:03:10 +0200

    lsh: forgot to add ifup and ifdown source files.

---
+++ apps/lsh/lsh_cmd_ifdown.c
@@ -0,0 +1,7 @@
+#include "config.h"
+#include "lsh_internal.h"
+
+void lsh_cmd_ifdown(void)
+{
+	lsh_puts("...\r\n");
+}
---
+++ apps/lsh/lsh_cmd_ifup.c
@@ -0,0 +1,7 @@
+#include "config.h"
+#include "lsh_internal.h"
+
+void lsh_cmd_ifup(void)
+{
+	lsh_puts("...\r\n");
+}
